24b681799b914884a64e2e2fa3a3e6717e3ee585
Lenovo <Lenovo@LAPTOP-LR60D6IR>
2023-01-22 08:28:18 +0530

    SSSshipco sunday project added in local repository

---
+++ src/Shipco_Array.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Shipco_Array {
+
+	public static void main(String[] args) {
+	ArrayList al=new ArrayList();
+			{
+				al.add("yuvraj");
+				al.add(15);
+				al.add(50.15);
+				System.out.println(al);
+			}
+}
+}
